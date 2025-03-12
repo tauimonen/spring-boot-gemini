dc91cbc77becdf4fcea9fff7143ba369a0b743b8
tauimonen <tauimonen@gmail.com>
2025-03-12 12:53:57 +0200

    Create a Spring Boot Project

---
+++ src/test/java/dev/tau/flash/FlashApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tau.flash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
