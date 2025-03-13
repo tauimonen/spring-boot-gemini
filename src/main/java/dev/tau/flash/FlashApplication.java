0b5c0db628e9ec63abaf642231e562af033acf9a
tauimonen <tauimonen@gmail.com>
2025-03-13 11:26:43 +0200

    Create a commandLineRunner

--- src/main/java/dev/tau/flash/FlashApplication.java
+++ src/main/java/dev/tau/flash/FlashApplication.java
@@ -1,7 +1,10 @@
 package dev.tau.flash;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlashApplication {
@@ -10,4 +13,15 @@ public class FlashApplication {
 		SpringApplication.run(FlashApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(ChatClient.Builder builder) {
+		return args -> {
+			var client = builder.build();
+			String response = client.prompt("What is the approximate value of pi to six decimal places?")
+					.call()
+					.content();
+
+			System.out.println(response);
+		};
+	}
 }
